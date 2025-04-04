ba5b7ef1b4ee7de940fb15e2b0889703b5537859
deepak-s-git <deepak.s.workdesk@gmail.com>
2025-04-05 00:26:04 +0530

    LEETCODE 121

--- Buy-Sell Stock l.py
+++ Buy-Sell Stock l.py
@@ -1,12 +1,15 @@
-l,r = 0,1       #LEFT AND RIGHT POINTER FOR A DAY AND SUCCEEDING DAY
-Max_Profit = 0
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
 
-while r < len(prices):
-    if prices[l] < prices[r]:         # IF THE NEXT DAY PRICE IS GREATER....CALCULATE PROFIT
-            Profit = prices[r] - prices[l]
-            Max_Profit = max(Max_Profit,Profit)
-    else:                             # IF THE NEXT DAY PRICE HAS DROPPED...INCREMENT THE RIGHT POINTER ONLY 
-        l = r
-    r += 1
-        
-return Max_Profit
+        l,r = 0,1      #LEFT AND RIGHT POINTER FOR A DAY AND SUCCEEDING DAY
+        Max_Profit = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:       # IF THE NEXT DAY PRICE IS GREATER....CALCULATE PROFIT
+                Profit = prices[r] - prices[l] 
+                Max_Profit = max(Max_Profit,Profit)
+            else:                             # IF THE NEXT DAY PRICE HAS DROPPED...INCREMENT THE RIGHT POINTER ONLY 
+                l = r
+            r += 1
+
+        return Max_Profit
