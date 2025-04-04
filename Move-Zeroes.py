3ce8ae792d5cde54f1fa638621ddae80f307e822
deepak-s-git <deepak.s.workdesk@gmail.com>
2025-04-04 22:39:45 +0530

    LEETCODE 283

---
+++ Move-Zeroes.py
@@ -0,0 +1,6 @@
+ n = len(nums)
+     i = 0
+     for j in range(n):
+        if (nums[j] != 0):      
+            nums[i],nums[j] = nums[j],nums[i]   #SWAP TE I AND J DATA IN IF J IS IN NON ZERO ELEMENT
+            i += 1
