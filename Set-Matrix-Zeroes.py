b74f1f72102c36cb8bde12c0f3fc3e9da2da8abf
deepak-s-git <deepak.s.workdesk@gmail.com>
2025-04-05 00:21:37 +0530

    LEETCODE 73

--- Set-Matrix-Zeroes.py
+++ Set-Matrix-Zeroes.py
@@ -1,29 +1,34 @@
-m = len(matrix)
-n = len(matrix[0])
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+      
 
-row_has_zero = False
-col_has_zero = False
+      m = len(matrix)
+      n = len(matrix[0])
 
-#ITERATE THROUGH MATRIX TO MARK THE ZERO ROWS OR COLUMNS
-for row in range(m):
-    for col in range(n):
-        if matrix[row][col] == 0:
-            if row == 0:
-                row_has_zero = True
-            if col == 0:
-                 col_has_zero = True
-            matrix[row][0] = matrix[0][col] = 0
+      row_has_zero = False
+      col_has_zero = False
 
-#ITERATE THROUGH THE MATRIX AGAIN TO UPDATE THE CELL TO BE ZERO IF ITS A ZERO ROW OR COLUMN
-for row in range(1,m):
-    for col in range(1,n):
-        matrix[row][col] = 0 if matrix[row][0] == 0 or matrix[0][col] == 0 else matrix[row][col]
+      #ITERATE THROUGH MATRIX TO MARK THE ZERO ROWS OR COLUMNS
+      for row in range(m):
+        for col in range(n):
+            if matrix[row][col] == 0:
+                if row == 0:
+                    row_has_zero = True
+                if col == 0:
+                    col_has_zero = True
+                matrix[row][0] = matrix[0][col] = 0
+      
+      #ITERATE THROUGH THE MATRIX AGAIN TO UPDATE THE CELL TO BE ZERO IF ITS A ZERO ROW OR COLUMN
+      for row in range(1,m):
+        for col in range(1,n):
+            matrix[row][col] = 0 if matrix[row][0] == 0 or matrix[0][col] == 0 else matrix[row][col]
 
-#UPDATE THE FIRST ROW AND COLUMN IF THEY ARE ZERO
-if row_has_zero:
-    for col in range(n):
-        matrix[0][col] = 0
+      #UPDATE THE FIRST ROW AND COLUMN IF THEY ARE ZERO
+      if row_has_zero:
+        for col in range(n):
+            matrix[0][col] = 0
 
-if col_has_zero:
-    for row in range(m):
-        matrix[row][0] = 0
+      if col_has_zero:
+        for row in range(m):
+            matrix[row][0] = 0
+        
