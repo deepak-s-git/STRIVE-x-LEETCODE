441dad8e3916e9f752ad2b4ff19383260208fce4
deepak-s-git <deepak.s.workdesk@gmail.com>
2025-04-05 00:07:38 +0530

    LEETCODE 121

---
+++ Buy-Sell Stock l.py
@@ -0,0 +1,12 @@
+l,r = 0,1       #LEFT AND RIGHT POINTER FOR A DAY AND SUCCEEDING DAY
+Max_Profit = 0
+
+while r < len(prices):
+    if prices[l] < prices[r]:         # IF THE NEXT DAY PRICE IS GREATER....CALCULATE PROFIT
+            Profit = prices[r] - prices[l]
+            Max_Profit = max(Max_Profit,Profit)
+    else:                             # IF THE NEXT DAY PRICE HAS DROPPED...INCREMENT THE RIGHT POINTER ONLY 
+        l = r
+    r += 1
+        
+return Max_Profit
