f4c662ebf54eac1b47716488457a56390b22c314
deepak-s-git <deepak.s.workdesk@gmail.com>
2025-04-05 00:23:47 +0530

    LEETCODE 283

--- Move-Zeroes.py
+++ Move-Zeroes.py
@@ -1,7 +1,9 @@
-n = len(nums)
-i = 0
-
-for j in range(n):
-    if (nums[j] != 0):      
-        nums[i],nums[j] = nums[j],nums[i]   #SWAP TE I AND J DATA IN IF J IS IN NON ZERO ELEMENT
-        i += 1
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+     
+     n = len(nums)
+     i = 0
+     for j in range(n):
+        if (nums[j] != 0):
+            nums[i],nums[j] = nums[j],nums[i]    #SWAP I AND J IF J IS IN NON ZERO ELEMENT
+            i += 1
